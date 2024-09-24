d3daeef6d8968650c25244dd3882d7c808cf2a49
Vito-Scaletta00 <jt.shaw667@gmail.com>
2024-09-24 20:37:53 +1000

    added comments

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,6 @@ import pygame
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -13,16 +12,16 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
-    def draw(self, screen):
+    def draw(self, screen): #handles drawing the player character
         pygame.draw.polygon(screen, (255,255,255),self.triangle(),2)
 
     def update(self, dt):
         # sub-classes must override
         pass
     
-    def collision(self,CircleShape):
+    def collision(self,CircleShape): 
         distance = self.position.distance_to(CircleShape.position)
-        if distance <= self.radius + CircleShape.radius:
+        if distance <= self.radius + CircleShape.radius:            #Detects collision and returns true/false, everything should use this for colliding
             return True
         else:
             return False
