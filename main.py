a680220de1ec8565dc98da6b6f7f7f5899ec99f3
Vito-Scaletta00 <jt.shaw667@gmail.com>
2024-09-24 20:38:31 +1000

    added splitting

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameClock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+
     AsteroidField.containers = (updatable,)
     Asteroid.containers = (asteroids,updatable,drawable)
     Player.containers = (updatable, drawable)
     Shot.containers = (shots,updatable, drawable)
+
     player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
     asteroidfield = AsteroidField()
     while True:
@@ -40,6 +43,9 @@ def main():
             if item.collision(player):
                 print ("Game Over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collision(item):
+                    item.split()
 
         pygame.display.flip()
 
