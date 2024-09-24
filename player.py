c8010074178de430225a38b99595190562b7cbeb
Vito-Scaletta00 <jt.shaw667@gmail.com>
2024-09-24 20:38:57 +1000

    shoot stuff

--- player.py
+++ player.py
@@ -2,10 +2,12 @@ from circleshape import *
 from constants import *
 from shot import *
 
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -29,6 +31,7 @@ class Player(CircleShape):
             
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer = (self.timer - dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -37,5 +40,6 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.timer < 0:
             self.shoot()
+            self.timer = PLAYER_SHOOT_COOLDOWN
