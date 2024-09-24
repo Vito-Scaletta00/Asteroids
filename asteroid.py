5a5ecb20a71d419f945066165eeb016665ca4905
Vito-Scaletta00 <jt.shaw667@gmail.com>
2024-09-24 19:13:30 +1000

    created asteroid.py, handles the appearance and update subclass

---
+++ asteroid.py
@@ -0,0 +1,10 @@
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+           super().__init__(x,y,radius)
+    def draw(self,screen):
+        return pygame.draw.circle(screen,(255,255,255),(self.position.x, self.position.y),self.radius,2)
+    def update(self, dt):
+         self.position.x += self.velocity.x * dt
+         self.position.y += self.velocity.y *dt
