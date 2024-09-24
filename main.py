4a30c0bad7eeab43682435043cbd568481b4bd9a
Vito-Scaletta00 <jt.shaw667@gmail.com>
2024-09-24 19:15:47 +1000

    Tons of stuff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -10,7 +14,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameClock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots,updatable, drawable)
     player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: ###Allows us to quit with the exit button
@@ -19,7 +32,15 @@ def main():
         dt = gameClock.tick(60) / 1000  #pauses the loops it's contained in until 1/60th of a second has passed, 
                                         #then returns the delta time and divides it by 1000 (converts milliseonds to seconds) 
                                         # and assigns this to the dt variable
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+        for item in updatable:
+            item.update(dt)
+        for item in asteroids:
+            if item.collision(player):
+                print ("Game Over!")
+                sys.exit()
+
         pygame.display.flip()
 
 
