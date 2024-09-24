521754af137a2dfcfeea64d60468aaae86abdc26
Vito-Scaletta00 <jt.shaw667@gmail.com>
2024-09-24 14:28:02 +1000

    Added circleshape.py and player.py modules, configured them to draw player on screen. Updated main to use these modules

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
@@ -8,12 +8,21 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameClock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: ###Allows us to quit with the exit button
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        pygame.Surface.fill(screen,(0,0,0)) #makes screen black
+        dt = gameClock.tick(60) / 1000  #pauses the loops it's contained in until 1/60th of a second has passed, 
+                                        #then returns the delta time and divides it by 1000 (converts milliseonds to seconds) 
+                                        # and assigns this to the dt variable
+        player.draw(screen)
         pygame.display.flip()
+
+
 
 
 
